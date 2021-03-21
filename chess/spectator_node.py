976fd0708d104441fb843dbfb918bfb74f52800d
Ryan Govostes <rgovostes@whoi.edu>
2021-03-21 15:30:18 +0000

    Improve visibility of pieces

--- chess/spectator_node.py
+++ chess/spectator_node.py
@@ -20,25 +20,31 @@ def handle_capture(player, capture):
 
 
 def prettyprint(board):
-    symbols = {
-        Player.WHITE: dict(zip('prnbqk', u'♙♖♘♗♕♔')),
-        Player.BLACK: dict(zip('prnbqk', u'♟♜♞♝♛♚')),
-    }
+    symbols = dict(zip('prnbqk', u'♟♜♞♝♛♚'))
+
+    # If these don't look good on your terminal, you can adjust them
+    # https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+    ansi_fg = { Player.WHITE: "38;5;240", Player.BLACK: "38;5;232" }
+    ansi_bg = [ "48;5;250", "48;5;255" ]  # black, white
 
     for row in xrange(7, -1, -1):  # draw row 1 last, at the bottom
         line = ''
         for col in range(8):
             piece = board.squares[8*row+col]
-            line += '\x1b[{}m'.format([47,107][(row + col) % 2])
-            line += ' ' + symbols[piece.player.id].get(piece.kind, ' ') + ' '
-            line += '\x1b[0m'
+            line += u'\x1b[{};{}m {} '.format(
+                ansi_fg[piece.player.id],
+                ansi_bg[(row + col) % 2],
+                symbols.get(piece.kind, ' '),
+            )
 
         if row == 7:
-            line += '  ' + ''.join(symbols[Player.WHITE][c.kind]
-                                   for c in captures[Player.BLACK])
-        if row == 0:
-            line += '  ' + ' '.join(symbols[Player.BLACK][c.kind]
-                                    for c in captures[Player.WHITE])
+            line += '\x1b[0;{}m  '.format(ansi_fg[Player.WHITE])
+            line += ''.join(symbols[c.kind] for c in captures[Player.BLACK])
+        elif row == 0:
+            line += '\x1b[0;{}m  '.format(ansi_fg[Player.BLACK])
+            line += ''.join(symbols[c.kind] for c in captures[Player.WHITE])
+        
+        line += '\x1b[0m'
         print(line)
     print()
 
